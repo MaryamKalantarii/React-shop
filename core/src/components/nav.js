b3949511da8a9ffd877b68a726a326f9b6bd427d
MaryamKalantarii <137641133+MaryamKalantarii@users.noreply.github.com>
2025-04-26 13:33:29 +0430

    nav css

--- core/src/components/nav.js
+++ core/src/components/nav.js
@@ -2,7 +2,13 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { useContext } from "react";
+import { ShopContext } from "../context/shopContext";
+import './nav.css'
 const Nav = ()=>{
+  const {cartItems} = useContext(ShopContext)
+  const itemCount = cartItems?.reduce((prev , current)=> {
+    return prev + current.count
+  },0)
     return(
         <div className="navbar navbar-dark bg-dark navbar-expand-lg">
       <div className="container">
@@ -16,7 +22,7 @@ const Nav = ()=>{
            <li className="nav-item">
             <Link to="/cart" className="nav-link">
               <FontAwesomeIcon icon={faShoppingCart}  />
-              {/* {itemCount > 0 && <span className="cart-items-count">{itemCount}</span>} */}
+              {itemCount > 0 && <span className="cart-items-count">{itemCount}</span>}
             </Link>
           </li> 
         </ul>
